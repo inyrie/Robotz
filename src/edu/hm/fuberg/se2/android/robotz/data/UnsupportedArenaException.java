e75234c98724003cc2a9afdcf6599f951413d442
Steff Ehrenberg <inyrie@web.de>
2013-04-18 12:56:40 +0200

    - added serialVersionUID for Exc. UnsupportedArenaException

---
+++ src/edu/hm/fuberg/se2/android/robotz/data/UnsupportedArenaException.java
@@ -0,0 +1,22 @@
+package edu.hm.fuberg.se2.android.robotz.data;
+
+public class UnsupportedArenaException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedArenaException() {
+
+		super();
+	}
+
+	/**
+	 * @param detailMessage
+	 */
+	public UnsupportedArenaException(final String message) {
+
+		super(message);
+	}
+}
