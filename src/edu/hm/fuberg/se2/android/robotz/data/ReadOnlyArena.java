0586a52347bbc88e4f462146031b5be658fecb53
Steff Ehrenberg <inyrie@web.de>
2013-04-19 16:20:20 +0200

    - forgot what exactly I did there. Just renaming/formatting stuff I guess -

--- src/edu/hm/fuberg/se2/android/robotz/data/ReadOnlyArena.java
+++ src/edu/hm/fuberg/se2/android/robotz/data/ReadOnlyArena.java
@@ -1,36 +1,33 @@
 package edu.hm.fuberg.se2.android.robotz.data;
 
 /**
- * The Interface ReadonlyItem restricts the access to Arena data for other layers.
+ * The Interface ReadOnlyItem restricts the access to Arena data for other
+ * layers.
  * @author Stephanie Ehrenberg
  * @author Robert Fuess
  */
 public interface ReadOnlyArena {
 
-	//	/**					//Zugriff noch unbekannt
-	//	 * Gets the item.
-	//	 *
-	//	 * @return the item
-	//	 */
-	//	Item getItem();
+	// Zugriff noch unbekannt!
+	/**
+	 * @return the item
+	 */
+	/* Item getItem(); */
 
 	/**
-	 * Gets the width of the Areana.
-	 *
+	 * Gets the width of the Arena.
 	 * @return the width of the Arena
 	 */
 	double getWidth();
 
 	/**
 	 * Gets the height of the Arena.
-	 *
 	 * @return the height of the Arena
 	 */
 	double getHeight();
 
 	/**
 	 * Gets the state of the Game.
-	 *
 	 * @return the state of the Game
 	 */
 	GameState getState();
--- src/edu/hm/fuberg/se2/android/robotz/data/ReadOnlyItem.java
+++ src/edu/hm/fuberg/se2/android/robotz/data/ReadOnlyItem.java
@@ -1,30 +1,28 @@
 package edu.hm.fuberg.se2.android.robotz.data;
 
 /**
- * The Interface ReadonlyItem restricts the access to Item data for other layers.
+ * The Interface ReadOnlyItem restricts the access to Item data for other
+ * layers.
  * @author Stephanie Ehrenberg
  * @author Robert Fuess
  */
-public interface ReadonlyItem {
+public interface ReadOnlyItem {
 
 	/**
 	 * Gets the X coordinate of the Item.
-	 *
-	 * @return the X coordinate of the Item
+	 * @return the X coordinate of the Item.
 	 */
 	double getXCoord();
 
 	/**
 	 * Gets the Y coordinate of the Item.
-	 *
-	 * @return the Y coordinate of the Item
+	 * @return the Y coordinate of the Item.
 	 */
 	double getYCoord();
 
 	/**
 	 * Gets the size of the Item.
-	 *
-	 * @return the size of the Item
+	 * @return The size of the Item.
 	 */
 	double getSize();
 
