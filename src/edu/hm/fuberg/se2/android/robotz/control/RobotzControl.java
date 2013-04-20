2884f4ccd6552da96209bdf8600b1daffd750ead
Steff Ehrenberg <inyrie@web.de>
2013-04-20 22:45:54 +0200

    - Added methods createNewTarget(), holdGame(), continueGame() to class RobotzControl; refactored setRobotzView(); minor JavaDoc stuff.

--- src/edu/hm/fuberg/se2/android/robotz/control/RobotzControl.java
+++ src/edu/hm/fuberg/se2/android/robotz/control/RobotzControl.java
@@ -1,18 +1,19 @@
 package edu.hm.fuberg.se2.android.robotz.control;
 
 import edu.hm.fuberg.se2.android.robotz.data.Arena;
+import edu.hm.fuberg.se2.android.robotz.data.Target;
 import edu.hm.fuberg.se2.android.robotz.view.UpdateOnlyView;
 
-public class RobotzControl implements UpdateOnlyView {
+public class RobotzControl {
 
-	/**  */
+	/** The robotzView object. */
 	private UpdateOnlyView robotzView;
 
 	/** The robotz data. */
 	private final Arena robotzData;
 
 	/**
-	 * Sets the robotsData.
+	 * Ctor.
 	 * @param robotzData the robotz data.
 	 */
 	public RobotzControl(final Arena robotzData) {
@@ -21,18 +22,43 @@ public class RobotzControl implements UpdateOnlyView {
 	}
 
 	/**
-	 * Sets the robotz view.
-	 * @param robotzView the new robotz view.
+	 * Sets the robotzView object.
+	 * @param robotzView The robotzView object.
 	 * @throws IllegalArgumentException if this.robotzView != null.
 	 */
 	public void setRobotzView(final UpdateOnlyView robotzView) {
 
-		if (this.robotzView != null) {
-
-			throw new IllegalArgumentException("view set twice");
+		if (this.robotzView == null) {
+			this.robotzView = robotzView;
+			new Updater(robotzView, robotzData).start();
 		}
+		else {
+			throw new IllegalArgumentException("View cannot be set twice!");
+		}
+	}
 
-		this.robotzView = robotzView;
-		new Updater(robotzView, robotzData).start();
+	/**
+	 * Method for setting a new target point.
+	 * @param destination
+	 * @return
+	 */
+	public Target createNewTarget(final Target destination) {
+		robotzData.getPlayer().setDestination(destination);
+		return destination;
+	}
+
+	/**
+	 * Method for freezing the game, for example when changing from Robotz App
+	 * to homescreen without closing the App completely.
+	 */
+	public void holdGame() {
+		// here be magical code!
+		// end Thread Updater.
+	}
+
+	/** Method for continuing a previously frozen game. */
+	public void continueGame() {
+		// here be magical code, too!
+		// create new Thread Updater!
 	}
 }
