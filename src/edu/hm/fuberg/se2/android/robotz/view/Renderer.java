c333278d12018e501792e98aee41f23746b1bce8
Doomed2Failure <Doomed2Failure@gmx.de>
2013-04-21 14:44:40 +0200

    Ctor added

--- src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
+++ src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
@@ -1,5 +1,7 @@
 package edu.hm.fuberg.se2.android.robotz.view;
 
+import android.view.SurfaceHolder;
+
 /**
  * Class is responsible for painting the Game.
  * @author Stephanie Ehrenberg
@@ -7,6 +9,23 @@ package edu.hm.fuberg.se2.android.robotz.view;
  * @version 2013-04-21
  */
 public class Renderer implements UpdateOnlyView {
+
+	private final int width;
+	private final int height;
+	private final SurfaceHolder holder;
+
+	/**
+	 * Ctor for a new Renderer object
+	 * @param width the width of the canvas in pixel.
+	 * @param height the height of the canvas in pixel.
+	 * @param holder the SurfaceHolder.
+	 */
+	public Renderer(final int width, final int height, final SurfaceHolder holder) {
+
+		this.width = width;
+		this.height = height;
+		this.holder = holder;
+	}
 
 	@Override
 	public void update() {
