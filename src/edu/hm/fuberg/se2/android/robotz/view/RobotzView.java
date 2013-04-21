8bbf2e87e808bad32886fa57c104d180552c97cc
Doomed2Failure <Doomed2Failure@gmx.de>
2013-04-21 11:30:43 +0200

    SurfaceHolder.Callback Interface Methods implemented, Ctor and OnTouch event defined.

--- src/edu/hm/fuberg/se2/android/robotz/view/RobotzView.java
+++ src/edu/hm/fuberg/se2/android/robotz/view/RobotzView.java
@@ -4,9 +4,12 @@
 package edu.hm.fuberg.se2.android.robotz.view;
 
 import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import edu.hm.fuberg.se2.android.robotz.control.RobotzControl;
 import edu.hm.fuberg.se2.android.robotz.data.ReadOnlyArena;
+import edu.hm.fuberg.se2.android.robotz.data.Target;
 
 /**
  * ...
@@ -14,19 +17,63 @@ import edu.hm.fuberg.se2.android.robotz.data.ReadOnlyArena;
  * @author Robert Fuess
  * @version 2013-04-21
  */
-public class RobotzView extends SurfaceView implements UpdateOnlyView {
+public class RobotzView extends SurfaceView implements SurfaceHolder.Callback, UpdateOnlyView {
+
+	/** The surface holder. */
+	private final SurfaceHolder surfaceHolder;
+
+	/** The robotz control object. */
+	private final RobotzControl robotzControl;
+
+	/** The robotz data object. */
+	private final ReadOnlyArena robotzData;
 
 	/**
-	 * @param context
+	 * Ctor for a new RobotzView.
+	 * @param control the robotz control object.
+	 * @param data the robotz data object.
+	 * @param context the context
 	 */
 	public RobotzView(final RobotzControl control, final ReadOnlyArena data, final Context context) {
+
 		super(context);
+		surfaceHolder = getHolder();
+		surfaceHolder.addCallback(this);
+		robotzControl = control;
+		robotzControl.setRobotzView(this);
+		robotzData = data;
 	}
 
-	/* (non-Javadoc)
-	 * @see edu.hm.fuberg.se2.android.robotz.view.UpdateOnlyView#update() */
-	@Override public void update() {
+	@Override
+	public void surfaceChanged(final SurfaceHolder holder, final int format, final int width,	final int height) {
 
 	}
 
+	@Override
+	public void surfaceCreated(final SurfaceHolder holder) {
+
+
+	}
+
+	@Override
+	public void surfaceDestroyed(final SurfaceHolder holder) {
+
+
+	}
+
+	@Override
+	public void update() {
+	}
+
+	@Override
+	public boolean onTouchEvent(final MotionEvent event) {
+
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+			// Anpassung auf Modellkoordinaten fehlt noch.
+			robotzControl.createNewTarget(new Target(event.getX(), event.getY()));
+		}
+
+		return true;
+	}
 }
