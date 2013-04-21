d04b9dee2dcb1e549049068faa91080692ebdab1
Doomed2Failure <Doomed2Failure@gmx.de>
2013-04-21 11:27:48 +0200

    initial commit

---
+++ src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
@@ -0,0 +1,14 @@
+package edu.hm.fuberg.se2.android.robotz.view;
+
+/**
+ * Class is responsible for painting the Game.
+ * @author Stephanie Ehrenberg
+ * @author Robert Fuess
+ * @version 2013-04-21
+ */
+public class Renderer implements UpdateOnlyView {
+
+	@Override
+	public void update() {
+	}
+}
