d695904922381c5546e8f409f58940f507585975
Steff Ehrenberg <inyrie@web.de>
2013-04-23 16:02:44 +0200

    - started on Testfile for class Arena. Looking goooood! ^^

---
+++ src/edu/hm/fuberg/se2/android/robotz/data/ArenaTest.java
@@ -0,0 +1,37 @@
+/**
+ *
+ */
+package edu.hm.fuberg.se2.android.robotz.data;
+
+/**
+ * @author Stäff
+ */
+public class ArenaTest {
+
+	public static void main(final String... ignored) {
+		// final Arena arena = new Arena(0, 0); // wirft USA-Exception!
+		final Arena arena = new Arena(20, 20);
+		assert arena.getWidth() == 20 : "Width should be 20.";
+		assert arena.getHeight() == 20 : "Height should be 20";
+		// assert arena.getExit() == null :
+		// "There should be no Exit object yet.";
+		assert arena.getExit().getXCoord() == 20 : "Exit's xCoordinate should be 20.";
+		assert arena.getExit().getYCoord() == 20 : "Exit's yCoordinate should be 20.";
+		assert arena.getPlayer().getXCoord() == 0 : "Player's xCoordinate should be 0.";
+		assert arena.getPlayer().getYCoord() == 0 : "Player's yCoordinate should be 0.";
+		assert arena.getPlayer().getDestination() == null : "Player shouldn't have a destination yet.";
+		assert arena.getState() == GameState.Waiting : "Gamestate should be Waiting.";
+		// final Player player = new Player(5, 5);
+		// arena.setPlayer(player);
+		arena.getPlayer().shift(5, 5);
+		assert arena.getPlayer().getVelocity() == 0.2 : "Player's velocity should be 0.2.";
+		final Exit exit = new Exit(20, 0);
+		arena.setExit(exit);
+		assert arena.getExit().getXCoord() == 20 : "Exit's xCoordinate should be 20.";
+		assert arena.getExit().getYCoord() == 0 : "Exit's yCoordinate should be 0.";
+		arena.getPlayer().move(10);
+		assert arena.getPlayer().getXCoord() == 5 : "Player's xCoordinate should be 5, he shouldn't have moved.";
+		assert arena.getPlayer().getYCoord() == 5 : "Player's yCoordinate should be 5, he shouldn't have moved.";
+		// arena.getPlayer()
+	}
+}
