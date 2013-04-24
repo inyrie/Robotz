eb6ca7722d474a7df0dd09ac0470be3567f65e80
Steff Ehrenberg <inyrie@web.de>
2013-04-24 11:01:16 +0200

    Added new tests.

--- src/edu/hm/fuberg/se2/android/robotz/data/ArenaTest.java
+++ src/edu/hm/fuberg/se2/android/robotz/data/ArenaTest.java
@@ -25,13 +25,16 @@ public class ArenaTest {
 		// arena.setPlayer(player);
 		arena.getPlayer().shift(5, 5);
 		assert arena.getPlayer().getVelocity() == 0.2 : "Player's velocity should be 0.2.";
-		final Exit exit = new Exit(20, 0);
+		final Exit exit = new Exit(20, 20);
 		arena.setExit(exit);
 		assert arena.getExit().getXCoord() == 20 : "Exit's xCoordinate should be 20.";
-		assert arena.getExit().getYCoord() == 0 : "Exit's yCoordinate should be 0.";
+		assert arena.getExit().getYCoord() == 20 : "Exit's yCoordinate should be 0.";
 		arena.getPlayer().move(10);
 		assert arena.getPlayer().getXCoord() == 5 : "Player's xCoordinate should be 5, he shouldn't have moved.";
 		assert arena.getPlayer().getYCoord() == 5 : "Player's yCoordinate should be 5, he shouldn't have moved.";
-		// arena.getPlayer()
+		arena.getPlayer().setDestination(exit);
+		arena.getPlayer().move(10);
+		assert arena.getPlayer().getXCoord() - 6.414 < 0.1 : "Player's xCoordinate should be 6.414.";
+		assert arena.getPlayer().getYCoord() - 6.414 < 0.1 : "Player's yCoordinate should be 6.414.";
 	}
 }
