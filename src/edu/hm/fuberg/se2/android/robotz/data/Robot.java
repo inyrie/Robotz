4056a098e2a851b4c0abd85171711b45419a995e
Steff Ehrenberg <inyrie@web.de>
2013-04-24 10:59:39 +0200

    Added Javadoc, refactored Ctor in class Robot

--- src/edu/hm/fuberg/se2/android/robotz/data/Robot.java
+++ src/edu/hm/fuberg/se2/android/robotz/data/Robot.java
@@ -7,15 +7,17 @@ package edu.hm.fuberg.se2.android.robotz.data;
  */
 public class Robot extends MobileItem {
 
+	/** Defines a robot's size. */
 	public static final int ROBOT_SIZE = 1;
+	/** Defines a robot's velocity. */
 	public static final int ROBOT_VELOCITY = 1;
 
 	/**
+	 * Ctor.
 	 * @param xCoordinate
 	 * @param yCoordinate
 	 */
 	public Robot(final double xCoordinate, final double yCoordinate) {
 		super(xCoordinate, yCoordinate, ROBOT_SIZE, ROBOT_VELOCITY);
 	}
-
 }
