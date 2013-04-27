e611fe01ddb96bcca0eb199b773eedc1bd58af9f
Steff Ehrenberg <inyrie@web.de>
2013-04-27 18:27:32 +0200

    Methods for computing pixel coordinates to model coordinates and vice versa.

--- src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
+++ src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
@@ -1,7 +1,11 @@
 package edu.hm.fuberg.se2.android.robotz.view;
 
+import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
+import edu.hm.fuberg.se2.android.robotz.data.Item;
 import edu.hm.fuberg.se2.android.robotz.data.ReadOnlyArena;
+import edu.hm.fuberg.se2.android.robotz.data.Target;
 
 /**
  * Class is responsible for painting the Game.
@@ -10,6 +14,9 @@ import edu.hm.fuberg.se2.android.robotz.data.ReadOnlyArena;
  * @version 2013-04-25
  */
 public class Renderer implements UpdateOnlyView {
+
+	/** */
+	private final Canvas canvas;
 
 	/** The width of the screen. */
 	private final int surfaceWidth;
@@ -25,7 +32,6 @@ public class Renderer implements UpdateOnlyView {
 
 	/**
 	 * Ctor for a new Renderer object.
-	 *
 	 * @param data the robotz data
 	 * @param holder the SurfaceHolder.
 	 * @param width the width of the screen.
@@ -37,11 +43,39 @@ public class Renderer implements UpdateOnlyView {
 		surfaceHolder = holder;
 		surfaceWidth = width;
 		surfaceHeight = height;
+		canvas = surfaceHolder.lockCanvas();
 	}
 
 	@Override public void update() {
 
 		// BULLSHIT CODE for PMD
-		System.out.println(surfaceWidth +  surfaceHeight + surfaceHolder.toString() + robotzData.toString());
+		System.out.println(surfaceWidth + surfaceHeight + surfaceHolder.toString() + robotzData.toString());
+	}
+
+	/**
+	 * Bla.
+	 * @param event Bla.
+	 * @return Bla.
+	 */
+	public Target pixelToModelCoords(final MotionEvent event) {
+
+		final double factorWidth = robotzData.getWidth() / canvas.getWidth();
+		final double factorHeight = robotzData.getHeight() / canvas.getHeight();
+
+		return new Target(event.getX() * factorWidth, event.getY() * factorHeight);
+	}
+
+	/**
+	 * Bla.
+	 * @param item Bla.
+	 * @return Bla.
+	 */
+	public double[] modelToPixelCoords(final Item item) {
+
+		final double factorWidth = canvas.getWidth() / robotzData.getWidth();
+		final double factorHeight = canvas.getHeight() / robotzData.getHeight();
+
+		// Returning the computed pixel coordinates as double[] array.
+		return new double[] {item.getXCoord() * factorWidth, item.getYCoord() * factorHeight};
 	}
 }
