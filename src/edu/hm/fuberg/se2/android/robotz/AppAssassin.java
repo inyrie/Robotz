f0f3244d21c7e2aad810ba42c81bd14610994a27
Steff Ehrenberg <inyrie@web.de>
2013-05-07 15:08:31 +0200

    Created class AppAssassin for terminating the app.

---
+++ src/edu/hm/fuberg/se2/android/robotz/AppAssassin.java
@@ -0,0 +1,47 @@
+/**
+ *
+ */
+package edu.hm.fuberg.se2.android.robotz;
+
+import android.app.Activity;
+import edu.hm.fuberg.se2.android.robotz.control.RobotzControl;
+
+/**
+ * @author Stäff
+ */
+public class AppAssassin extends Thread {
+
+	/** The Activity object. */
+	final Activity activity;
+	/** The robotzControl object. */
+	final RobotzControl robotzControl;
+
+	/**
+	 * Ctor.
+	 * @param activityObject The Activity object.
+	 * @param controlObject The RobotzControl object.
+	 */
+	public AppAssassin(final Activity activityObject, final RobotzControl controlObject) {
+
+		activity = activityObject;
+		robotzControl = controlObject;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run() */
+	@Override public void run() {
+		synchronized (robotzControl) {
+
+			try {
+				robotzControl.wait();
+			}
+
+			catch (final InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			activity.finish();
+		}
+	}
+
+}
