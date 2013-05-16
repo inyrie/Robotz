5c596d134b2cab6c1b10c236ba83091d4985d51e
Steff Ehrenberg <inyrie@web.de>
2013-05-17 01:39:19 +0200

    Moved calculating/converting methods from class Renderer to extra class Converter.

--- src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
+++ src/edu/hm/fuberg/se2/android/robotz/view/Renderer.java
@@ -1,21 +1,29 @@
+/**
+ * Munich University for Applied Science,
+ * Faculty 07 for Mathematics and Computer Science
+ * Softwareentwicklung II, SS2013, Studiengruppe IF1A
+ * Windows XP SP3; Java-Version: 1.7.0_17
+ * Developing a Java application.
+ */
+
 package edu.hm.fuberg.se2.android.robotz.view;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import edu.hm.fuberg.se2.android.robotz.data.Item;
 import edu.hm.fuberg.se2.android.robotz.data.ReadOnlyArena;
-import edu.hm.fuberg.se2.android.robotz.data.Target;
 
 /**
  * Class is responsible for painting the Game.
  * @author Stephanie Ehrenberg
  * @author Robert Fuess
- * @version 2013-04-25
+ * @version 2013-05-17
  */
 public class Renderer implements UpdateOnlyView {
+
+	// /////////////// OBJECT VARIABLES /////////////////
 
 	/** The surface holder. */
 	private final SurfaceHolder surfaceHolder;
@@ -23,17 +31,10 @@ public class Renderer implements UpdateOnlyView {
 	/** The robotz data. */
 	private final ReadOnlyArena robotzData;
 
-	/** Defines the factor for the width to calculate from model to pixel coordinates. */
-	private final double modelToPixelFactorX;
+	/** A converter object, responsible for converting model to pixel values and vice versa. */
+	private final Converter converter;
 
-	/** Defines the factor for the height to calculate from model to pixel coordinates. */
-	private final double modelToPixelFactorY;
-
-	/** Defines the factor for the width to calculate from pixel to model coordinates. */
-	private final double pixelToModelFactorX;
-
-	/** Defines the factor for the height to calculate from pixel to model coordinates. */
-	private final double pixelToModelFactorY;
+	// /////////////// C T O R /////////////////
 
 	/**
 	 * Ctor for a new Renderer object.
@@ -42,17 +43,23 @@ public class Renderer implements UpdateOnlyView {
 	 * @param width the width of the screen.
 	 * @param height the height of the screen.
 	 */
-	public Renderer(final ReadOnlyArena data, final SurfaceHolder holder, final int width, final int height) {
-
-		final double surfaceSizePixel = Math.min(width, height);
+	Renderer(final ReadOnlyArena data, final SurfaceHolder holder, final int width, final int height) {
 
 		robotzData = data;
 		surfaceHolder = holder;
-		modelToPixelFactorX = surfaceSizePixel / data.getWidth();
-		modelToPixelFactorY = surfaceSizePixel / data.getHeight();
-		pixelToModelFactorX = data.getWidth() / surfaceSizePixel;
-		pixelToModelFactorY = data.getHeight() / surfaceSizePixel;
+		converter = new Converter(robotzData, Math.min(width, height));
 	}
+
+	// /////////////// G E T T E R /////////////////
+
+	/**
+	 * @return the converter
+	 */
+	Converter getConverter() {
+		return converter;
+	}
+
+	// /////////////// ///////////////// /////////////////
 
 	@Override public void update() {
 		final Canvas canvas = surfaceHolder.lockCanvas();
@@ -69,17 +76,19 @@ public class Renderer implements UpdateOnlyView {
 		}
 	}
 
+	// /////////////// DRAWING METHODS /////////////////
+
 	/**
 	 * Method draws a player on the canvas.
 	 * @param drawCanvas the canvas.
 	 */
-	public void drawPlayer(final Canvas drawCanvas) {
+	private void drawPlayer(final Canvas drawCanvas) {
 
 		final Item player = robotzData.getPlayer();
 		final double halfSize = player.getSize() / 2;
-		final float radius = modelToPixel(halfSize);
+		final float radius = converter.modelToPixelValues(halfSize);
 
-		final double[] playerCoords = modelToPixelCoords(player, halfSize);
+		final double[] playerCoords = converter.modelToPixelCoords(player, halfSize);
 		drawCanvas.drawCircle((float) playerCoords[0], (float) playerCoords[1], radius, defineBrush(Color.GREEN));
 	}
 
@@ -87,13 +96,13 @@ public class Renderer implements UpdateOnlyView {
 	 * Method draws an exit on the canvas.
 	 * @param drawCanvas the canvas.
 	 */
-	public void drawExit(final Canvas drawCanvas) {
+	private void drawExit(final Canvas drawCanvas) {
 
 		final Item exit = robotzData.getExit();
 		final double halfSize = exit.getSize() / 2;
-		final float radius = modelToPixel(halfSize);
+		final float radius = converter.modelToPixelValues(halfSize);
 
-		final double[] exitCoords = modelToPixelCoords(exit, halfSize);
+		final double[] exitCoords = converter.modelToPixelCoords(exit, halfSize);
 		drawCanvas.drawCircle((float) exitCoords[0], (float) exitCoords[1], radius, defineBrush(Color.BLUE));
 	}
 
@@ -101,15 +110,15 @@ public class Renderer implements UpdateOnlyView {
 	 * Method draws a Target on the canvas.
 	 * @param drawCanvas the canvas.
 	 */
-	public void drawTarget(final Canvas drawCanvas) {
+	private void drawTarget(final Canvas drawCanvas) {
 
 		if (robotzData.getPlayer().getDestination() != null) {
 
 			final Item target = robotzData.getPlayer().getDestination();
 			final double halfSize = target.getSize() / 2;
-			final float radius = modelToPixel(halfSize);
+			final float radius = converter.modelToPixelValues(halfSize);
 
-			final double[] targetCoords = modelToPixelCoords(target, halfSize);
+			final double[] targetCoords = converter.modelToPixelCoords(target, halfSize);
 			drawCanvas.drawCircle((float) targetCoords[0], (float) targetCoords[1], radius, defineBrush(Color.WHITE));
 		}
 	}
@@ -118,14 +127,14 @@ public class Renderer implements UpdateOnlyView {
 	 * Method draws a robot on the canvas.
 	 * @param drawCanvas the canvas.
 	 */
-	public void drawRobots(final Canvas drawCanvas) {
+	private void drawRobots(final Canvas drawCanvas) {
 
 		for (int position = 0; position < robotzData.getAmountRobots(); position++) {
 
 			final Item robot = robotzData.getRobot(position);
 			final double halfSize = robot.getSize() / 2;
-			final float radius = modelToPixel(halfSize);
-			final double[] robotCoords = modelToPixelCoords(robot, halfSize);
+			final float radius = converter.modelToPixelValues(halfSize);
+			final double[] robotCoords = converter.modelToPixelCoords(robot, halfSize);
 			drawCanvas.drawCircle((float) robotCoords[0], (float) robotCoords[1], radius, defineBrush(Color.RED));
 		}
 	}
@@ -134,94 +143,27 @@ public class Renderer implements UpdateOnlyView {
 	 * Method draws a fence on the canvas.
 	 * @param drawCanvas the canvas.
 	 */
-	public void drawFences(final Canvas drawCanvas) {
+	private void drawFences(final Canvas drawCanvas) {
 
 		for (int position = 0; position < robotzData.getAmountFences(); position++) {
 
 			final Item fence = robotzData.getFence(position);
 			final double halfSize = fence.getSize() / 2;
-			final float radius = modelToPixel(halfSize);
+			final float radius = converter.modelToPixelValues(halfSize);
 
-			final double[] fenceCoords = modelToPixelCoords(fence, halfSize);
+			final double[] fenceCoords = converter.modelToPixelCoords(fence, halfSize);
 			drawCanvas.drawCircle((float) fenceCoords[0], (float) fenceCoords[1], radius, defineBrush(Color.YELLOW));
 		}
 	}
 
 	/**
-	 * Method calculates the pixel coordinates to logical coordinates.
-	 * @param event The touch event.
-	 * @return the logical coordinates.
-	 */
-	Target pixelToModelCoords(final MotionEvent event) {
-
-		final double halfSize = Target.TARGET_SIZE / 2;
-		final double modelX = event.getX() * pixelToModelFactorX - halfSize;
-		final double modelY = event.getY() * pixelToModelFactorY - halfSize;
-		final double modelSize = Math.min(robotzData.getHeight(), robotzData.getWidth());
-
-		if (modelX < modelSize && modelY < modelSize) {
-
-			return new Target(modelX, modelY);
-		}
-
-		else {
-
-			if (robotzData.getPlayer().getDestination() == null) {
-
-				return null;
-			}
-
-			else {
-
-				return new Target(robotzData.getPlayer().getDestination().getXCoord(), robotzData.getPlayer()
-						.getDestination().getYCoord());
-			}
-		}
-	}
-
-	// /**
-	// * Method calculates the logical coordinates to pixel coordinates.
-	// * @param item The item with logical coordinates.
-	// * @return The pixel coordinates as array.
-	// */
-	// private double[] modelToPixelCoords(final Item item) {
-	//
-	// // Returning the computed pixel coordinates as double[] array.
-	// return new double[] {item.getXCoord() * modelToPixelFactorX, item.getYCoord() * modelToPixelFactorY};
-	// }
-
-	/**
-	 * Method calculates the logical coordinates to pixel coordinates.
-	 * @param item The item with logical coordinates.
-	 * @param shift The delta for which the coordinates are shifted.
-	 * @return The pixel coordinates as array.
-	 */
-	private double[] modelToPixelCoords(final Item item, final double shift) {
-
-		// Returning the computed pixel coordinates as double[] array.
-
-		return new double[] {(item.getXCoord() + shift) * modelToPixelFactorX,
-				(item.getYCoord() + shift) * modelToPixelFactorY};
-	}
-
-	/**
-	 * Method for computing pixel values from model values (used for drawing).
-	 * @param modelValue The model value to convert into pixel values.
-	 * @return The pixel values.
-	 */
-	private float modelToPixel(final double modelValue) {
-		return (float) (modelValue * Math.min(modelToPixelFactorX, modelToPixelFactorY));
-	}
-
-	/**
-	 * Support method for defining the Paint object with which to draw on the canvas.
+	 * Method for defining the Paint object with which to draw on the canvas.
 	 * @param color The color to draw.
 	 * @return Returns a Paint object.
 	 */
 	private Paint defineBrush(final int color) {
 
 		final Paint paint = new Paint();
-
 		paint.setColor(color);
 
 		return paint;
