cc113c7b348aab68735f86615d2158033f9843e4
Steff Ehrenberg <inyrie@web.de>
2013-05-20 19:38:51 +0200

    Last-minute Javadoc.

--- src/edu/hm/fuberg/se2/android/robotz/RobotzActivity.java
+++ src/edu/hm/fuberg/se2/android/robotz/RobotzActivity.java
@@ -21,7 +21,7 @@ import edu.hm.fuberg.se2.android.robotz.view.RobotzView;
  * The Class initiates the Game.
  * @author Stephanie Ehrenberg
  * @author Robert Fuess
- * @version 2013-05-19
+ * @version 2013-05-20
  */
 public class RobotzActivity extends Activity {
 
@@ -34,19 +34,23 @@ public class RobotzActivity extends Activity {
 	@Override protected void onCreate(final Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
+		// setup for app window
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+		// instanciating a config object that handles the game setup
 		final GameConfig configurator = new GameConfig(this);
 		Arena robotzData;
 
 		try {
-			robotzData = new Arena(configurator.getGameboard(),configurator.getSpeedPlayer(), configurator.getSpeedRobot());
+			robotzData = new Arena(configurator.getGameboard(), configurator.getSpeedPlayer(),
+					configurator.getSpeedRobot());
 			final RobotzControl robotzControl = new RobotzControl(robotzData);
 			robotzView = new RobotzView(this, robotzControl, robotzData);
 
 			setContentView(robotzView);
 
+			// instanciating and starting a new assassin thread that handles the ending of the activity
 			assassin = new AppAssassin(this, robotzControl);
 			assassin.start();
 		}
@@ -58,9 +62,15 @@ public class RobotzActivity extends Activity {
 
 	@Override protected void onDestroy() {
 		super.onDestroy();
+		// onDestroy() also triggers the assassin to finish the activity correctly
 		assassin.interrupt();
 	}
 
+	/**
+	 * Redefinition of onConfigurationChanged to handle orientation changes that occur when the smart device is rotated.
+	 * Causes the activity to pause.
+	 * @param newConfig The new Configuration object containing the performed changes.
+	 */
 	@Override public void onConfigurationChanged(final Configuration newConfig) {
 
 		super.onConfigurationChanged(newConfig);
