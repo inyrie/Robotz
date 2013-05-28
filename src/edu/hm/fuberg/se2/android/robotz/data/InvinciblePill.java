a9c5e2fd0fe7b63f45268f3b9bb7f8e9c64e2df1
Inyrie <inyrie@web.de>
2013-05-28 22:48:40 +0200

    Added getter, setter and decrement method for pill countdown.

--- src/edu/hm/fuberg/se2/android/robotz/data/InvinciblePill.java
+++ src/edu/hm/fuberg/se2/android/robotz/data/InvinciblePill.java
@@ -8,16 +8,21 @@
 
 package edu.hm.fuberg.se2.android.robotz.data;
 
+import android.util.Log;
+
 /**
  * The Class describes the invincible pill of the Game.
  * @author Stephanie Ehrenberg
  * @author Robert Fuess
- * @version 2013-05-24
+ * @version 2013-05-28
  */
 public class InvinciblePill extends Item {
 
 	/** The size of the invincible pill. */
-	public static final double INVINCIBLE_PILL_SIZE = 0.85;
+	static final double INVINCIBLE_PILL_SIZE = 0.85;
+
+	/** Time in ms for the pill's lifespan. It defines how long a generated invincible pill is active on the gameboard. */
+	private int pillCountdown = 20000;
 
 	/**
 	 * Ctor for a new invincible pill object.
@@ -26,5 +31,34 @@ public class InvinciblePill extends Item {
 	 */
 	public InvinciblePill(final double xCoord, final double yCoord) {
 		super(xCoord, yCoord, INVINCIBLE_PILL_SIZE);
+	}
+
+	// ///////////////////// G E T T E R & S E T T E R ///////////////////////
+
+	public int getPillCountdown() {
+		return pillCountdown;
+	}
+
+	void setPillCountdown(final int newValue) {
+		pillCountdown = newValue;
+	}
+
+	// ///////////////////// C O U N T D O W N - M E T H O D S ///////////////////////
+
+	/**
+	 * @param deltaTime
+	 */
+	public void decrementCountdown(final int deltaTime) {
+
+		int newTimeLeft = getPillCountdown() - deltaTime;
+
+		// Pill lifetime left stops at zero.
+		if (newTimeLeft < 0) {
+			newTimeLeft = 0;
+		}
+
+		setPillCountdown(newTimeLeft);
+
+		Log.d("robotz_invincible", "decrementCountdown() - pillCountdown = " + getPillCountdown());
 	}
 }
